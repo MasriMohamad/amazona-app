cc50be9e08f31844ac607a2c46d6b0eb8aa3cfba
MOHAMAD MASRI <almasri.mohamad@outlook.com>
2021-11-15 22:09:30 +0200

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,84 @@
+import React from "react";
+const data={
+    products:[
+        {
+            _id:"1",
+            name:"Nike Slim Shirt",
+            category:"Shirts",
+            image:"/images/shirt.jpeg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality product"
+        },
+        {
+            _id:"2",
+            name:"Adidas Fit Shirt",
+            category:"Shirts",
+            image:"/images/shirt.jpeg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality product"
+        }
+        ,
+        {
+            _id:"3",
+            name:"Nike Slim Shirt",
+            category:"Shirts",
+            image:"/images/shirt.jpeg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality product"
+        },
+        {
+            _id:"4",
+            name:"Nike Slim Shirt",
+            category:"Shirts",
+            image:"/images/shirt.jpeg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality product"
+        },
+        {
+            _id:"5",
+            name:"Nike Slim Shirt",
+            category:"Shirts",
+            image:"/images/shirt.jpeg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality product"
+        },
+        {
+            _id:"6",
+            name:"Nike Slim Shirt",
+            category:"Shirts",
+            image:"/images/shirt.jpeg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality product"
+        },
+        {
+            _id:"7",
+            name:"Nike Slim Shirt",
+            category:"Shirts",
+            image:"/images/shirt.jpeg",
+            price:120,
+            brand:"Nike",
+            rating:4.5,
+            numReviews:10,
+            description:"High quality product"
+        }
+    ]
+}
+export default data;
